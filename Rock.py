bf164f3b38db153f2f6892df777efc7880d625b6
marigab002 <gabwconta@gmail.com>
2024-10-17 16:44:30 -0300

    Add files via upload

---
+++ Rock.py
@@ -0,0 +1,19 @@
+Numero1 = float(input("Digite o número: "))
+Numero2 = float(input("Digite o número: "))
+Numero3 = float(input("Digite o número: "))
+Quantidade_de_número = float(input("Digite o número: "))
+
+Resultado= Numero1 + Numero2 + Numero3 / Quantidade_de_número
+
+Aprovado = Resultado >=7
+Reprovado = Resultado <=7
+
+#Função de resultado
+
+def Resultado ():
+
+Resultados = "Aprovado" if >=7 or "Reprovado" if <=7
+
+
+print(Resultado)
+
